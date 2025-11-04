c8822a34610b88bd542472909d492f7696633aaa
Suryaa <suryaacoolness@gmail.com>
2025-11-03 19:17:45 -0500

    google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-3XR61KN7TB"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-3XR61KN7TB');
+            `,
+          }}
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
